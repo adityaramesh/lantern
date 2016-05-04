6f1e73bfaa065c93eb16026029fe2427514bbaf1
Aditya Ramesh <_@adityaramesh.com>
2016-05-04 02:25:02 -0400

    Using fail_if.

--- lantern/utility/filesystem.lua
+++ lantern/utility/filesystem.lua
@@ -6,21 +6,9 @@ local F = flow.F
 local os = flow.os
 local lfs = flow.lfs
 
-local function fatal_error(msg, logger)
-	if logger ~= nil then
-		logger:log('/console/error', msg)
-		os.exit(1)
-	else
-		error(msg)
-	end
-end
-
 function lantern.remove_file(path, logger)
 	local success, err = os.remove(path)
-
-	if not success then
-		fatal_error(F"Failed to remove file '{path}': {err}", logger)
-	end
+	lantern.fail_if(not success, F"Failed to remove file '{path}': {err}", logger)
 end
 
 function lantern.remove_file_if_exists(path, logger)
@@ -29,15 +17,10 @@ function lantern.remove_file_if_exists(path, logger)
 end
 
 function lantern.rename_file(old_path, new_path, logger)
-	if paths.filep(new_path) then
-		fatal_error(F"Path '{new_path}' already exists.", logger)
-	end
-
+	lantern.fail_if(paths.filep(new_path), F"Path '{new_path}' already exists.", logger)
 	local success, err = os.rename(old_path, new_path)
-
-	if not success then
-		fatal_error(F"Failed to rename '{old_path}' to '{new_path}': {err}", logger)
-	end
+	lantern.fail_if(not success, F"Failed to rename '{old_path}' to '{new_path}': {err}",
+		logger)
 end
 
 function lantern.rename_file_if_exists(old_path, new_path, logger)
@@ -47,18 +30,12 @@ end
 
 function lantern.create_hard_link(src_path, dst_path, logger)
 	local success, err = lfs.link(src_path, dst_path)
-
-	if not success then
-		fatal_error(F"Failed creating hard link '{dst_path}': {err}", logger)
-	end
+	lantern.fail_if(not success, F"Failed creating hard link '{dst_path}': {err}", logger)
 end
 
 function lantern.make_directory(dir, logger)
 	local success, err = lfs.mkdir(dir)
-
-	if not success then
-		fatal_error(F"Failed to make directory '{dir}': {err}", logger)
-	end
+	lantern.fail_if(not success, F"Failed to make directory '{dir}': {err}", logger)
 end
 
 function lantern.make_directory_if_not_exists(dir, logger)
@@ -68,10 +45,7 @@ end
 
 function lantern.remove_empty_directory(dir, logger)
 	local success, err = lfs.rmdir(dir)
-
-	if not success then
-		fatal_error(F"Failed to remove directory '{dir}': {err}", logger)
-	end
+	lantern.fail_if(not success, F"Failed to remove directory '{dir}': {err}", logger)
 end
 
 function lantern.remove_directory(dir, logger)
