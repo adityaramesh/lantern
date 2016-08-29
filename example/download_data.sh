9b2438941ea301b8372e270c43ba1ff943661d7b
Aditya Ramesh <_@adityaramesh.com>
2016-08-28 20:41:39 -0400

    Why did I decide to use bash for anything

--- example/download_data.sh
+++ example/download_data.sh
@@ -3,10 +3,10 @@
 mkdir -p data/mnist/padded
 cd data/mnist/padded
 
-if [[! -f train.hdf5 ]]; then
+if [[ ! -f train.hdf5 ]]; then
 	wget https://github.com/adityaramesh/lantern/releases/download/data/train.hdf5
 fi
 
-if [[! -f test.hdf5 ]]; then
+if [[ ! -f test.hdf5 ]]; then
 	wget https://github.com/adityaramesh/lantern/releases/download/data/test.hdf5
 fi
